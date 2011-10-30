c89f3502c7a2bca35345655967139f40d93b6962
thiago <thiago@thiago-VirtualBox.(none)>
2011-10-30 15:16:22 -0200

    restaurando

---
+++ analisadorLexico.cpp
@@ -0,0 +1,244 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <ctype.h>
+#include "token.cpp"
+#include "readbuffer.cpp"
+#include "keywordlist.cpp"
+using namespace std;
+
+enum lexState {S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12, 
+			   S13, S14};
+
+class AnalisadorLexico {
+	private:
+		ReadBuffer ReadBuf;
+		string FileName;
+		string DataBlock;
+		string Buffer;
+		lexState Estado;	
+		bool ValidToken;
+		void CheckAddToBuffer(char SimboloEsp, char Simbol);
+		tokenType CurrentTokenType;
+		KeywordList palavrasReservadas;		
+	protected:
+		bool IsValidChar(char Value);
+		void Automato(char Value);
+		void TrataIdentificador(char Value);
+		void TrataDigito(char Value);
+		void TrataSimbolo(char Simbolo);
+		void TrataUmSimbolo(char Simbolo);
+		void TrataDoisSimbolo(char Simbolo);
+ 
+		void TokenFound();		
+	public:
+		AnalisadorLexico(string NomeArquivo);
+		~AnalisadorLexico();
+		void setFileName(string Value);
+		string getFileName();				
+		Token* getToken();
+		bool SourceEOF();
+};
+
+AnalisadorLexico::AnalisadorLexico(string NomeArquivo){
+	this->setFileName(NomeArquivo);
+	Estado = S0;
+	Buffer = "";
+	DataBlock = "";	
+	ValidToken = false;
+	CurrentTokenType = ttNone;
+	this->ReadBuf.OpenFile(NomeArquivo);
+}
+
+AnalisadorLexico::~AnalisadorLexico(){	
+	this->ReadBuf.CloseFile();
+}
+
+void AnalisadorLexico::setFileName(string Value)
+{
+	this->FileName = Value;
+}
+
+bool AnalisadorLexico::SourceEOF(){
+	return not(this->ReadBuf.IsFileGood());
+}
+
+string AnalisadorLexico::getFileName()
+{
+	return this->FileName;
+}
+
+bool AnalisadorLexico::IsValidChar(char Value){
+	bool isValid = false;
+	isValid = (Value!=' ');
+	isValid = int(Value) > 32;
+	return isValid;
+}
+
+Token* AnalisadorLexico::getToken(){
+	TokenFactory tkFactory;		
+	Token* returnToken = NULL;
+	
+	while ((this->ReadBuf.IsFileGood())and(not(this->ValidToken))){	
+		this->Automato(this->ReadBuf.getChar());
+	}
+	
+	if (this->ValidToken){
+		returnToken = tkFactory.getToken(this->CurrentTokenType, 
+			this->ReadBuf.getBuffer(), this->ReadBuf.getLineNumber(), 
+			(this->ReadBuf.getColNumber() - (this->ReadBuf.getBuffer().size()-2)));
+	}
+	
+	this->ValidToken = false;
+	return returnToken;
+}
+
+
+void AnalisadorLexico:: TrataUmSimbolo(char Simbolo){
+	switch (Simbolo){
+		case '.': this->ReadBuf.AddToBuffer(Simbolo);break; 
+		case ':': this->ReadBuf.AddToBuffer(Simbolo);break;
+		case '=': this->ReadBuf.AddToBuffer(Simbolo);break;
+		case '-': this->ReadBuf.AddToBuffer(Simbolo);break;
+		case ',': this->ReadBuf.AddToBuffer(Simbolo);break;  
+		case ';': this->ReadBuf.AddToBuffer(Simbolo);break;  
+		case '(': this->ReadBuf.AddToBuffer(Simbolo);break; 
+		case ')': this->ReadBuf.AddToBuffer(Simbolo);break; 
+		case '[': this->ReadBuf.AddToBuffer(Simbolo);break; 
+		case ']': this->ReadBuf.AddToBuffer(Simbolo);break; 
+		case '#': this->ReadBuf.AddToBuffer(Simbolo);break;   
+	    case '<': this->ReadBuf.AddToBuffer(Simbolo);break;
+	    case '>': this->ReadBuf.AddToBuffer(Simbolo);break;  
+	    case '&': this->ReadBuf.AddToBuffer(Simbolo);break;   
+	    case '|': this->ReadBuf.AddToBuffer(Simbolo);break;  
+	    case '~': this->ReadBuf.AddToBuffer(Simbolo);break;  
+	    case '+': this->ReadBuf.AddToBuffer(Simbolo);break;   	    
+	    case '*': this->ReadBuf.AddToBuffer(Simbolo);break;   
+	    case '/': this->ReadBuf.AddToBuffer(Simbolo);break;   
+	    case '\'':this->ReadBuf.AddToBuffer(Simbolo);break;
+	    default: //this->TokenFound();
+	    break; 
+	}
+}
+
+void AnalisadorLexico::TrataDoisSimbolo(char Simbolo){
+	switch (Simbolo){
+		case '.': {this->CheckAddToBuffer('.',Simbolo); break;}
+		
+		case '=': { if (this->ReadBuf.getLastCharOnBuffer() == '<'){
+					  this->CheckAddToBuffer('<',Simbolo); 		
+					}
+				  else{ if (this->ReadBuf.getLastCharOnBuffer() == '>'){
+						   this->CheckAddToBuffer('>',Simbolo);
+						}
+					  else{if (this->ReadBuf.getLastCharOnBuffer() == ':'){
+							  this->CheckAddToBuffer(':',Simbolo);
+						     }
+				          } 		  
+		 			  }
+		break;}
+		
+		case '-': {this->CheckAddToBuffer('-',Simbolo); break;}
+		
+		case ']': {this->CheckAddToBuffer('[',Simbolo); break;} 
+	    default: this->TokenFound();
+	    break; 
+	}
+}
+
+
+void AnalisadorLexico::CheckAddToBuffer(char SimboloEsp,char Simbol){
+	if (this->ReadBuf.getLastCharOnBuffer()==SimboloEsp){
+		this->ReadBuf.AddToBuffer(Simbol);
+	}
+	else{
+		this->TokenFound();
+	}
+}
+
+void AnalisadorLexico::TrataSimbolo(char Simbolo){
+	if (this->ReadBuf.IsBufferWriteEmpty()){
+		this->TrataUmSimbolo(Simbolo);
+	}
+	else{
+		if(this->ReadBuf.getBuffer().size() == 1){
+			this->TrataDoisSimbolo(Simbolo);
+ 		}
+		else{
+			this->TokenFound();
+		}
+	}
+}
+	
+void AnalisadorLexico::TokenFound(){
+	ValidToken = true;
+	Estado = S0;	
+	this->ReadBuf.PriorColNumber();	
+	
+	if(this->palavrasReservadas.isKeyWord(this->ReadBuf.getBuffer())){
+		this->CurrentTokenType = ttKeyword;
+	}
+}
+
+void AnalisadorLexico::TrataIdentificador(char Value){
+	if ((isalpha(Value))or(isdigit(Value))){
+		this->ReadBuf.AddToBuffer(Value);
+	}else{
+		if ((Value=='_')and(this->ReadBuf.getLastCharOnBuffer()!='_')){
+			this->ReadBuf.AddToBuffer(Value);			
+		}else{	
+			//devolve identificador no buffer e volta pra S0
+			this->TokenFound();
+		}
+	}	
+}
+
+void AnalisadorLexico::TrataDigito(char Value){
+	if (isdigit(Value)){
+		this->ReadBuf.AddToBuffer(Value);
+	}else{
+		//devolve digito no buffer e volta pra S0
+		this->TokenFound();
+	}	
+}
+
+
+void AnalisadorLexico::Automato(char Value){
+	switch (Estado) {
+		case S0:{
+			if (this->IsValidChar(Value)){
+				this->ReadBuf.ClearBuffer();
+				if (isalpha(Value)){		
+					this->CurrentTokenType = ttId;			
+					Estado = S1;					
+					this->Automato(Value);
+				}else{
+					if (isdigit(Value)){						
+						this->CurrentTokenType = ttNumber;
+						Estado = S2;												
+						this->Automato(Value);
+					}else{
+						this->CurrentTokenType = ttSymbol;
+						Estado = S3;
+						this->Automato(Value);
+					}
+				}
+			}			
+			break;
+		}
+		case S1:{
+			this->TrataIdentificador(Value);
+			break;
+		}
+		case S2:{
+			this->TrataDigito(Value);
+			break;
+		}		
+		case S3:{
+			this->TrataSimbolo(Value);
+			break;
+		}
+		default: break;
+	}
+}
+
---
+++ readbuffer.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+using namespace std;
+
+class ReadBuffer {
+	private:
+		ifstream SourceFile;
+		int FilePos;
+		int BufferPos;
+		int LineNumber;
+		int ColNumber;
+		string BufferRead;
+		string BufferWrite;
+	protected:
+		void LoadBuffer();
+		bool IsBufferEmpty();
+	public:
+		ReadBuffer();	
+		~ReadBuffer();
+		int getLineNumber();
+		int getColNumber();		
+		void OpenFile(string SourceName);
+		void CloseFile();
+		void AddToBuffer(char Value);
+		void ClearBuffer();
+		void PriorColNumber();
+		bool IsFileOpen();
+		bool IsFileGood();
+		bool IsBufferWriteEmpty();
+		string getBuffer();	
+		char getLastCharOnBuffer();
+		char getChar();
+		 
+};
+
+ReadBuffer::ReadBuffer(){	
+	this->FilePos = 0;
+	this->BufferPos = 0;		
+	this->LineNumber = -1;
+	this->ColNumber = -1;
+	this->BufferRead = "";
+	this->BufferWrite = "";
+}
+
+ReadBuffer::~ReadBuffer(){
+	if (this->IsFileOpen()){
+		this->SourceFile.close();	
+	}
+}
+
+int ReadBuffer::getLineNumber(){
+	return this->LineNumber;
+}
+
+int ReadBuffer::getColNumber(){
+	return this->ColNumber;
+}		
+
+void ReadBuffer::OpenFile(string SourceName){
+	this->SourceFile.open(SourceName.c_str());	
+}
+
+void ReadBuffer::CloseFile(){
+	this->SourceFile.close();
+}
+
+void ReadBuffer::PriorColNumber(){
+	this->ColNumber--;
+}
+
+bool ReadBuffer::IsFileOpen(){
+	return (this->SourceFile.is_open());
+}
+
+bool ReadBuffer::IsBufferEmpty(){
+	return (this->BufferRead.empty());
+}
+
+bool ReadBuffer::IsFileGood(){
+	return (this->SourceFile.good());
+}
+
+void ReadBuffer::LoadBuffer(){
+	getline(this->SourceFile, this->BufferRead);
+	this->ColNumber = -1;
+	this->LineNumber++;	
+}
+
+char ReadBuffer::getChar(){
+	if ((this->IsBufferEmpty())or(this->ColNumber==this->BufferRead.size())){
+		this->LoadBuffer();
+	}	
+	this->ColNumber++;
+	return this->BufferRead[this->ColNumber];
+}
+
+char ReadBuffer::getLastCharOnBuffer(){
+	return this->BufferWrite[this->BufferWrite.size()-1];
+}
+
+void ReadBuffer::AddToBuffer(char Value){
+	this->BufferWrite += Value;
+}
+
+void ReadBuffer::ClearBuffer(){
+	this->BufferWrite = "";
+}
+
+string ReadBuffer::getBuffer(){
+	return this->BufferWrite;
+}
+
+bool ReadBuffer::IsBufferWriteEmpty(){
+	return (this->BufferWrite.empty());
+}
+
